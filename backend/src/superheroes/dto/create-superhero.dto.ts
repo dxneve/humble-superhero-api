4f140a13ad3ee607c4d1a5069a8ba028c36ca60e
dxneve <denevee@pm.me>
2025-02-08 04:25:20 +0200

    update CreateSupeheroDto with validation decorators

--- backend/src/superheroes/dto/create-superhero.dto.ts
+++ backend/src/superheroes/dto/create-superhero.dto.ts
@@ -1,1 +1,14 @@
-export class CreateSuperheroDto {}
+import { IsInt, IsString, Max, Min } from 'class-validator';
+
+export class CreateSuperheroDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  superpower: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(10)
+  humility: number;
+}
